6d6822657fe4c15ca7b5d8a00ff8df80384e6bcd
Raul Salcedo Garcia <rsalcedo@atll2000.es>
2024-01-28 20:46:26 +0100

    prueba usaurio

--- FACT_sales_rau.py
+++ FACT_sales_rau.py
@@ -15,6 +15,8 @@ query = "SELECT * FROM DIM_ITEMS"
 items = pd.read_sql_query(query,connection)
 result = pd.merge(sales, items, on='item', how='inner')
 del sales
+print("Primera etapa")
+
 query = "SELECT d, date, yearweek FROM daily_calendar"
 fechas = pd.read_sql_query(query,connection)
 fechas['id_date'] = pd.to_datetime(fechas['date'], format='%Y-%m-%d')
@@ -23,14 +25,14 @@ del result
 del fechas
 col_to_drop = ['d','date']
 result2 = result2.drop(col_to_drop, axis=1)
-
+print("Segunda etapa")
 
 query = "SELECT * FROM DIM_STORES"
 stores = pd.read_sql_query(query,connection)
 result3 = pd.merge(result2, stores, on='store_code', how='inner')
 del stores
 del result2
-
+print("Tercera etapa")
 
 query = "SELECT * FROM item_prices"
 prices = pd.read_sql_query(query,connection)
@@ -38,10 +40,12 @@ result4 = pd.merge(result3,prices, left_on=['yearweek','store_code','item'], rig
 del prices
 col_to_drop = ['item','yearweek','category','store_code']
 result4 = result4.drop(col_to_drop, axis=1)
+print("Cuarta etapa")
 
 result4 = result4.reset_index(drop=False)
 result4['total'] = result4['sales']*result4['sell_price']
 result4 = result4.rename(columns={'index':'id_sale'})
+print("Última etapa")
 
 result4.to_sql(name='FACT_SALES', con=connection, if_exists='replace', index=False)
 connection.commit()
